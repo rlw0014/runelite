5c4fdb7160009c29d021bf5688fc14b79845bef7
Ryan <rlw0014@gmail.com>
2019-05-10 22:01:00 -0400

    Update Game & Wave to show blue if positive

--- runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/Game.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/Game.java
@@ -7,6 +7,7 @@ import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
 import net.runelite.client.eventbus.Subscribe;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Game
@@ -96,9 +97,11 @@ public class Game
                 message.append("(");
                 if (totalPoints[i] < 0)
                 {
-                    message.append(ChatColorType.HIGHLIGHT);
-                    message.append(String.valueOf(totalPoints[i]));
-                    message.append(ChatColorType.NORMAL);
+                    message.append(Color.RED, String.valueOf(totalPoints[i]));
+                }
+                else if (totalPoints[i] > 0)
+                {
+                    message.append(Color.BLUE, String.valueOf(totalPoints[i]));
                 }
                 else
                 {
