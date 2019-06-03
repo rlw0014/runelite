b55c0637a0798c08a3782d027f1fe4d2f41d82da
Ryan <rlw0014@gmail.com>
2019-06-03 13:07:56 -0400

    Fix BarbarianAssault for launcher

--- runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/Game.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/Game.java
@@ -15,7 +15,7 @@ public class Game
 {
     private Client client;
     private String currentWave;
-    private ArrayList<Wave> waves = new ArrayList<>();
+    private ArrayList<Wave> Waves = new ArrayList<>();
     private String[] totalDescriptions = {
             "A: ",
             "; D: ",
@@ -37,10 +37,10 @@ public class Game
     {
         this.client = client;
     }
-    Game(Client client, ArrayList<Wave> waves)
+    Game(Client client, ArrayList<Wave> Waves)
     {
         this.client = client;
-        this.waves = waves;
+        this.Waves = Waves;
     }
     @Subscribe
     public void onChatMessage(ChatMessage chatMessage)
@@ -53,7 +53,7 @@ public class Game
         }
         if (currentWave.equals("1"))
         {
-            waves = null;
+            Waves = null;
             totalPoints = new int[6];
             totalAmounts = new int[6];
         }
@@ -65,7 +65,7 @@ public class Game
         int[] otherRolesPointsList;
         ChatMessageBuilder message = new ChatMessageBuilder();
         message.append("Round points: ");
-        for (Wave w : waves)
+        for (Wave w : Waves)
         {
             amountsList = w.getWaveAmounts();
             pointsList = w.getWavePoints();
